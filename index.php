9f7251dc14be6a09223edfa0b3ed6ec8edf65664
palach <ishop@MacBook-Air-Ishop.local>
2021-05-13 19:43:31 +0300

    added index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*$username = "Лиля Красотулька";
+
+
+
+echo $username;
+
+echo "<br>";
+
+$current_user = $username;
+
+
+echo $current_user;
+
+echo "<br>";
+
+
+$count = 28;
+
+echo $count
+*/
+
+
+
+
+
+
+
+
+?>
