dc7e0a5abaad327d07befd28ed524e621bb57495
palach <ishop@MacBook-Air-Ishop.local>
2021-05-17 15:12:23 +0300

    первое задание блока

---
+++ practice_7/logic_variables.php
@@ -0,0 +1,23 @@
+<?php
+
+//Если переменная $var равна true, то выведите 'True', иначе выведите 'False'. Проверьте работу скрипта при $var, равном true, false
+
+
+$var = true;
+
+if ($var == true) {
+    echo true;
+} else {
+    echo false;
+}
+
+
+echo '</br>';
+
+
+
+
+
+
+
+?>
