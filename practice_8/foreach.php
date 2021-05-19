af393d09996406835cc55938c3014f1b852770fa
palach <ishop@MacBook-Air-Ishop.local>
2021-05-19 17:58:17 +0300

    первое задание

---
+++ practice_8/foreach.php
@@ -0,0 +1,24 @@
+<?php
+
+//Дан массив с элементами 'html', 'css', 'php', 'js', 'jq'. С помощью цикла foreach выведите эти слова в столбик.
+
+$arr = ['html', 'css', 'php', 'js', 'jq'];
+
+
+
+foreach ($arr as $el) {
+    echo $el;
+    echo '</br>';
+} 
+    
+
+
+
+
+
+
+
+
+
+
+?>
