f86d2bf0f6c1973df64a6366423c822543ee51a4
palach <ishop@MacBook-Air-Ishop.local>
2021-05-19 17:31:05 +0300

    первое задание

--- practice_8/switch.php
+++ practice_8/switch.php
@@ -1,9 +1,25 @@
 <?php
 
+//Переменная $num может принимать одно из значений: 1, 2, 3 или 4. Если она имеет значение 1, то в переменную $result запишем ‘Winter’, если имеет значение 2 – ‘Spring’ и так далее
 
+$num = '1';
 
+switch ($num) {
+    case '1':
+        $result = 'winter';
+        break;
+    case '2':
+        $result = 'spring';
+        break;
+    case '3':
+        $result = 'summer';
+        break;
+    case '4':
+        $result = 'autumn';
+        break;            
+}
 
-
+echo $result
 
 
 
