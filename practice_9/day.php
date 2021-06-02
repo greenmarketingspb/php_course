8619885e0f301974ef12b1e5895e36e65957b0fb
palach <ishop@MacBook-Air-Ishop.local>
2021-06-02 17:34:01 +0300

    last version of calendar

--- practice_9/day.php
+++ practice_9/day.php
@@ -1,4 +1,11 @@
 <?php
 
-echo 'Day';
+$day = isset($_GET['dayOfMonth']) ? $_GET['dayOfMonth'] : date('d');
+$month = date('m');
+$year = date('Y');
+$currentTimestamp = mktime(0, 0, 0, $month, $day, $year);
+$dayOfWeek = date('l', $currentTimestamp);
+$fullDate = date('d-m-Y', $currentTimestamp);
+
+echo "The Day is $dayOfWeek -  $fullDate";
 
--- practice_9/month.php
+++ practice_9/month.php
@@ -2,10 +2,6 @@
 <table>
 
 
-
-
-
-
 <?php
 
 $year = date('Y');
@@ -16,10 +12,29 @@ $month = date('m');
 
 $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
+
+$week = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
+];
+
 echo '<tr>';
 
+
+foreach ($week as $dayName) {
+    echo "<td>$dayName</td>";
+}
+
+
+echo '<tr></tr>';
+
 for ($day = 1; $day <= $daysInMonth; $day++) {
-    echo "<td>$day</td>";
+    echo "<td><a href=day?dayOfMonth=$day>$day</a></td>";
     if ($day % 7 === 0) {
         echo '<tr></tr>';
 
