5f11b37f95bd3ccc05b8de95033295184ba43707
palach <ishop@MacBook-Air-Ishop.local>
2021-06-05 23:01:47 +0300

    formtest2

---
+++ practice_10/formtest2.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+} else $name = "(Не введено)";
+
+echo <<<_END
+<html>
+  <head>
+    <title>Form test</title 
+  </head>
+<body>
+Вас зовут: $name<br>
+<form method="post" action="formtest2.php">
+  Как вас зовут?
+  <input type="text" name="name">
+  <input type="submit">
+</form>
+</body>  
+</html>
+_END;
+?>
