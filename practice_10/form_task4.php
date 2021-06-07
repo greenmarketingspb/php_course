388a4874a45a5cece917197cfdd6af8c32cd8cda
palach <ishop@MacBook-Air-Ishop.local>
2021-06-07 19:52:04 +0300

    finish home work with forms

--- practice_10/form_task4.php
+++ practice_10/form_task4.php
@@ -1,16 +1,20 @@
 <?php
 
-if (isset($_POST['log']) && isset($_POST['password'])) {
-  $log = $_POST['log'];
-  $password = $_POST['password'];
-}
- 
 $login = 13;
 $pass = 13;
 
-if ($login === $log && $pass === $password) {
+if (isset($_POST['log']) && isset($_POST['password'])) {
+  $log = (int) $_POST['log'];
+  $password = (int) $_POST['password'];
+  if ($login === $log && $pass === $password) {
     echo 'Доступ разрешен!';
-} else echo 'Доступ запрещен!';
+} else {
+echo 'Доступ запрещен!';
+}
+
+}
+
+ 
 
 
 echo <<<_END
@@ -22,19 +26,15 @@ echo <<<_END
     <body>
       <form method="post" action="form_task4.php"><pre>
       Login <input type="text" name="log" required='required'>
-      Password <input type="text" name="password" required='required'>
+      Password <input type="password" name="password" required='required'>
       <input type="submit">
       </pre></form>
     </body>
 </html>
 _END;
 
-$login = 13;
-$pass = 13;
 
-if ($login === $log && $pass === $password) {
-    echo 'Доступ разрешен!';
-} else echo 'Доступ запрещен!';
+
 
 
 ?>
